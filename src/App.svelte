3b67ca5a9d4f27556958b155b46b50b9d07ccd3d
AirOne01 <popgthyrd@gmail.com>
2021-11-09 12:29:54 +0100

    🏗️ Rebuilt the header bar entirely, as the old one wasn't working

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
 	import Header from './Header.svelte'
 	import Three from './Three.svelte'
+	import Tests from './Tests.svelte'
+
+	let pfp_url = "";
+	let pfp_name = "test alt";
 </script>
 
 <main>
-	<Header/>
+	<Tests {pfp_url} {pfp_name}/>
 	<Three/>
 </main>
 
 <style>
 	main {
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		padding: 0;
+		margin: 0;
 	}
 
 	@media (min-width: 640px) {
--- src/Three.svelte
+++ src/Three.svelte
@@ -1,4 +1,6 @@
-<script>
+<script lang="ts">
+import { claim_component } from "svelte/internal";
+
   import {
     AmbientLight,
     BoxBufferGeometry,
@@ -15,36 +17,35 @@
   let cubeMaterial = new MeshStandardMaterial();
 </script>
 
-<main>
-  <Canvas let:sti w={100} h={100}>
-    <Scene {sti} let:scene id="scene1" props={{ background: 0xedf2f7 }}>
-      <PerspectiveCamera {scene} id="cam1" pos={[0, 0, 3]} lookAt={[0, 0, 0]} />
-      <AmbientLight {scene} intensity={1.25} />
-      <DirectionalLight {scene} pos={[3, 3, 3]} />
-      <Mesh
-        {scene}
-        geometry={cubeGeometry}
-        material={cubeMaterial}
-        mat={{ roughness: 0.5, metalness: 0.5, color: 0xff3e00 }}
-        pos={[0, 0, 0]}
-        rot={[0.5, 0.6, 0]}
-        scale={[1, 1, 1]} />
-    </Scene>
-    <WebGLRenderer
-      {sti}
-      sceneId="scene1"
-      camId="cam1"
-      config={{ antialias: true, alpha: false }} />
-  </Canvas>
-</main>
+<Canvas id="canvas" let:sti w={500} h={500}>
+  <Scene {sti} let:scene id="scene1" props={{ background: 0x000 }}>
+    <PerspectiveCamera {scene} id="cam1" pos={[0, 0, 3]} lookAt={[0, 0, 0]} />
+    <AmbientLight {scene} intensity={1.25} />
+    <DirectionalLight {scene} pos={[3, 3, 3]} />
+    <Mesh
+      {scene}
+      geometry={cubeGeometry}
+      material={cubeMaterial}
+      mat={{ roughness: 0.5, metalness: 0.5, color: 0xff3e00 }}
+      pos={[0, 0, 0]}
+      rot={[0.5, 0.6, 0]}
+      scale={[1, 1, 1]} />
+  </Scene>
+  <WebGLRenderer
+    {sti}
+    sceneId="scene1"
+    camId="cam1"
+    config={{ antialias: true, alpha: false }} />
+</Canvas>
 
 <style>
-  main {
+  #canvas {
     top: 0;
     left: 0;
     height: 100vh;
     width: 100vw;
     margin: 0;
+    padding: 0;
     background-color: red;
   }
 </style>
